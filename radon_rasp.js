b47b7ebafc8ba96bc107e454e02d8631faed6f5b
duckheewon <fain9301@yahoo.com>
2018-02-05 18:22:57 +0900

    test1

--- radon_rasp.js
+++ radon_rasp.js
@@ -1,4 +1,21 @@
 var Serialport = require('serialport');
 var radontest = require('./test1');
+var test2 = require('./test2');
 
-setInterval(function() { radontest.list(Serialport) }, 5000);
+var port = new Serialport('/dev/ttyUSB0', {
+    baudRate: 9600
+});
+var parsers = Serialport.parsers;
+var parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+port.pipe(parser);
+
+port.on('open', function() {
+    console.log('open port !');
+});
+
+//setInterval(function() { radontest.list(Serialport) }, 5000);
+
+setInterval(function() { test2.list(port, parser) }, 5000);
