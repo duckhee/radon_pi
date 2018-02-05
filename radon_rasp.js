145e241a63fd2dfc7e0bbabaaa688c234cf40afb
woosj417 <woosj417@gmail.com>
2018-02-05 17:34:17 +0900

    test

--- radon_rasp.js
+++ radon_rasp.js
@@ -1,5 +1,5 @@
 var Serialport = require('serialport');
-var radontest = require('./sensor_command');
+//var radontest = require('./sensor_command');
 
 
 var parsers = Serialport.parsers;
@@ -7,20 +7,25 @@ var parser = new parsers.Readline({
     delimiter: '\r\n'
 });
 
-var port = new Serialport('/dev/', {
-    baudrate:9600
+var port = new Serialport('/dev/ttyUSB0', {
+    baudRate:9600
 });
-
-
+console.log('TEST');
 port.pipe(parser);
+port.on('open', function(){
 
-port.write('p', function(data){
-    if(data){
-    console.log(data);
-    }
+	port.write('p', function(data){
+	    if(data){
+	    console.log(data);
+	    }
+	});
 });
 
+parser.on('data', function(data){
+	console.log(data);
+
+});
 
 port.on('error', function(err){
     console.log(err);
-})
+});
