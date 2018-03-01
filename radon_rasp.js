14d1bba95db959b565592e2b20fd64026c7d9fe7
duckhee <fain9301@yahoo.com>
2018-03-01 15:49:53 +0900

    testing radon get data

--- radon_rasp.js
+++ radon_rasp.js
@@ -11,26 +11,15 @@ var parser = new parsers.Readline({
 });
 
 port.pipe(parser);
-<<<<<<< HEAD
-port.on('open', function(){
-port.write('p', function(data){
-    if(data){
-    console.log(data);
-    }
-});
+port.on('open', function() {
+    port.write('p', function(data) {
+        if (data) {
+            console.log(data);
+        }
+    });
 
 });
 
-port.on('error', function(err){
+port.on('error', function(err) {
     console.log(err);
 });
-=======
-
-port.on('open', function() {
-    console.log('open port !');
-});
-
-//setInterval(function() { radontest.list(Serialport) }, 5000);
-
-setInterval(function() { test2.get_serial(port, parser) }, 5000);
->>>>>>> 44bc57cf4941a09d07758b592c76aeec7936e8d8
